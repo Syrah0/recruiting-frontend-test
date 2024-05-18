03bee5d78aa9d41bc03f8e662f99d219adc76c94
Alexandra <alexandra.ibarra.c@gmail.com>
2024-05-17 20:58:52 -0400

    [UPDATE] delete comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-// import logo from './logo.svg';
 import './App.css';
-// import { useState, useEffect } from 'react';
-// import { getInvoicesByType } from './services/invoices';
 
 import InvoiceProvider from './context/invoices';
 import InvoiceTable from './components/invoiceTable';
@@ -9,15 +6,6 @@ import CreditNoteTable from './components/creditNoteTable';
 import InfoModal from './components/infoModal';
 
 function App() {
-//  const [invoiceData, setInvoiceData] = useState([]);
-//  //// const [invoiceId, setInvoiceId] = useState(null);
-//
-//  //useEffect(() => {
-//  //  const response = getInvoicesByType({ type: 'received' });
-//  //  setInvoiceData(response);
-//  //}, []);
-//
-  //console.log(invoiceData);
 
   return (
     <InvoiceProvider>
